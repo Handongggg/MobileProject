211a6867fb6e45f174f7523fd8328967444eab18
DongyeonHan <hdy0409@ajou.ac.kr>
2019-05-06 16:59:47 +0900

    Initial commit

---
+++ app/src/main/java/com/example/opdshe/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.opdshe;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ImageButton Imagebutton_texi = (ImageButton) findViewById(R.id.texi);
+        ImageButton Imagebutton_bus = (ImageButton) findViewById(R.id.bus);
+        ImageButton Imagebutton_working = (ImageButton) findViewById(R.id.working);
+
+        Imagebutton_bus.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(getApplicationContext(), bus.class);
+                startActivity(intent);
+            }
+        });
+
+        Imagebutton_texi.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(getApplicationContext(), texi.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/opdshe/bus.java
@@ -0,0 +1,51 @@
+package com.example.opdshe;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class bus extends AppCompatActivity {
+    Toolbar toolbar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bus);
+
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        toolbar.setTitle("BUS");
+        toolbar.setTitleTextColor(Color.WHITE);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                finish();
+                return true;
+            case R.id.menu_search:
+                Toast.makeText(getApplicationContext()," press search",Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.menu_account:
+                Toast.makeText(getApplicationContext()," press account",Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.menu_logout:
+                Toast.makeText(getApplicationContext()," press logout",Toast.LENGTH_SHORT).show();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
