2105df7a462108437ece231d6a157202ac2a4b01
dongheon <opdshe@naver.com>
2019-05-09 19:50:28 +0900

    add Walking.class

---
+++ app/src/main/java/com/example/opdshe/Walking.java
@@ -0,0 +1,65 @@
+package com.example.opdshe;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class Walking extends AppCompatActivity {
+    Toolbar toolbar;
+    Button posting;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_taxi);
+        posting=findViewById(R.id.btn_posting);
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        toolbar.setTitle("Walking");
+        toolbar.setTitleTextColor(Color.WHITE);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        ActionBar ab = getSupportActionBar() ;
+        posting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(),TaxiPosting.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                finish();
+                return true;
+            case R.id.menu_search:
+                Toast.makeText(getApplicationContext()," press search",Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.menu_account:
+                Toast.makeText(getApplicationContext()," press account",Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.menu_logout:
+                Toast.makeText(getApplicationContext()," press logout",Toast.LENGTH_SHORT).show();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
