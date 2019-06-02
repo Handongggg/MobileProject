a1ed07a117e288935ab11001ba013ac9d431b6eb
leedongheon <rlfjrlck@ajou.ac.kr>
2019-06-02 20:32:08 +0900

    kakao_login

--- app/src/main/java/com/example/opdshe/Login.java
+++ app/src/main/java/com/example/opdshe/Login.java
@@ -2,18 +2,32 @@ package com.example.opdshe;
 
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.view.View;
+import android.widget.Toast;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.Api;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -21,16 +35,39 @@ import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.GoogleAuthProvider;
+import com.kakao.auth.ErrorCode;
+import com.kakao.auth.ISessionCallback;
+import com.kakao.auth.Session;
+import com.kakao.network.ErrorResult;
+import com.kakao.usermgmt.UserManagement;
+import com.kakao.usermgmt.callback.MeResponseCallback;
+import com.kakao.usermgmt.response.model.UserProfile;
+import com.kakao.util.exception.KakaoException;
 
-public class Login extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener{
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Login extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
+    //google
     SignInButton Google_Login;
-    private static final int RC_SIGN_IN = 100;
+    private static final int RC_SIGN_IN = 1000;
     private FirebaseAuth mAuth;
     private GoogleApiClient mGoogleApiClient;
+    //kakao
+    private static final String TAG = "";
+    SessionCallback callback;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        //getHashKey();
+        //kakao
+        callback = new SessionCallback();
+        Session.getCurrentSession().addCallback(callback);
+        requestMe();
+
+
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestIdToken(getString(R.string.default_web_client_id))
                 .requestEmail()
@@ -39,7 +76,9 @@ public class Login extends AppCompatActivity implements GoogleApiClient.OnConnec
                 .enableAutoManage(this, this)
                 .addApi(Auth.GOOGLE_SIGN_IN_API,gso)
                 .build();
+
         mAuth = FirebaseAuth.getInstance();
+
         Google_Login = findViewById(R.id.Google_Login);
         Google_Login.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,16 +88,19 @@ public class Login extends AppCompatActivity implements GoogleApiClient.OnConnec
             }
         });
     }
+
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == RC_SIGN_IN) {
             GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
             if (result.isSuccess()) {
+                //구글 로그인 성공해서 파베에 인증
                 GoogleSignInAccount account = result.getSignInAccount();
                 firebaseAuthWithGoogle(account);
             }
             else{
+                //구글 로그인 실패
             }
         }
     }
@@ -76,7 +118,104 @@ public class Login extends AppCompatActivity implements GoogleApiClient.OnConnec
                     }
                 });
     }
+
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
+
+
+
+
+
+    ////////////kakao
+    private void getHashKey(){
+        try {                                                        // 패키지이름을 입력해줍니다.
+            PackageInfo info = getPackageManager().getPackageInfo("com.example.opdshe", PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("temp","key_hash="+ Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+    }
+    class SessionCallback implements ISessionCallback {
+
+        @Override
+        public void onSessionOpened() {
+
+            UserManagement.requestMe(new MeResponseCallback() {
+                @Override
+                public void onFailure(ErrorResult errorResult) {
+                    String message = "failed to get user info. msg=" + errorResult;
+
+                    ErrorCode result = ErrorCode.valueOf(errorResult.getErrorCode());
+                    if (result == ErrorCode.CLIENT_ERROR_CODE) {
+                        //에러로 인한 로그인 실패
+                        // finish();
+                    } else {
+                        //redirectMainActivity();
+                    }
+                }
+
+                @Override
+                public void onSessionClosed(ErrorResult errorResult) {
+                }
+
+                @Override
+                public void onNotSignedUp() {
+
+                }
+
+                @Override
+                public void onSuccess(UserProfile userProfile) {
+                    //로그인에 성공하면 로그인한 사용자의 일련번호, 닉네임, 이미지url등을 리턴합니다.
+                    //사용자 ID는 보안상의 문제로 제공하지 않고 일련번호는 제공합니다.
+
+                     //Log.e("UserProfile", userProfile.toString());
+                     //Log.e("UserProfile", userProfile.getId() + "");
+
+
+                    long number = userProfile.getId();
+
+
+                }
+            });
+
+        }
+        // 세션 실패시
+        @Override
+        public void onSessionOpenFailed(KakaoException exception) {
+
+        }
+    }
+    public void requestMe() {
+        //유저의 정보를 받아오는 함수
+        UserManagement.requestMe(new MeResponseCallback() {
+            @Override
+            public void onFailure(ErrorResult errorResult) {
+                Log.e(TAG, "error message=" + errorResult);
+//                super.onFailure(errorResult);
+            }
+            @Override
+            public void onSessionClosed(ErrorResult errorResult) {
+                Log.d(TAG, "onSessionClosed1 =" + errorResult);
+            }
+            @Override
+            public void onNotSignedUp() {
+                //카카오톡 회원이 아닐시
+                Log.d(TAG, "onNotSignedUp ");
+            }
+            @Override
+            public void onSuccess(UserProfile result) {
+                Log.e("UserProfile", result.toString());
+                Log.e("UserProfile", result.getId() + "");
+            }
+        });
+    }
+
 }
